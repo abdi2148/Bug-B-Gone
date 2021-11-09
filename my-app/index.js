aed350776d50d45f267bcceee62ace3e2a090973
abdi2148 <abdiqafar.abas@gmail.com>
2021-11-09 11:10:29 +0100

    mainview changes and sidebar added

--- my-app/index.js
+++ my-app/index.js
@@ -17,7 +17,7 @@ function createWindow () {
     }
 })
 
-    win.loadFile('src/main-page.html')
+    win.loadFile('src/frontend/main-page/main-page.html')
     win.setBackgroundColor('#343B48')
 
     //// CLOSE APP
