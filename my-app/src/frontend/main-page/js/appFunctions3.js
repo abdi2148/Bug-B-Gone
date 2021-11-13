e44fa1d118975cb59638c0b7695604bf0f2b71a8
Jonas Spatzek <jona81e1@easv365.dk>
2021-11-13 15:40:45 +0100

    Close minimize maximize works

--- my-app/src/frontend/main-page/js/appFunctions3.js
+++ my-app/src/frontend/main-page/js/appFunctions3.js
@@ -1,9 +1,47 @@
-let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".sidebarBtn");
-sidebarBtn.onclick = function() {
-  sidebar.classList.toggle("active");
-  if(sidebar.classList.contains("active")){
-  sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
-}else
-  sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+const { ipcRenderer } = require('electron')
+const maxResBtn = document.getElementById('maxResBtn')
+const mySidebar = document.getElementById('mySidebar')
+const ipc = ipcRenderer
+var isLeftMenuActive = true
+
+
+//// MINIMIZE APP
+minimizeBtn.addEventListener('click', ()=>{
+    ipc.send('minimizeApp')
+})
+
+//// MAXIMIZE RESTORE APP
+function changeMaxResBtn(isMaximizedApp){
+    if(isMaximizedApp){
+        maxResBtn.title = 'Restore'
+        maxResBtn.classList.remove('maximizeBtn')
+        maxResBtn.classList.add('restoreBtn')
+    } else {
+        maxResBtn.title = 'Maximize'
+        maxResBtn.classList.remove('restoreBtn')
+        maxResBtn.classList.add('maximizeBtn')
+    }
 }
+maxResBtn.addEventListener('click', ()=>{
+    ipc.send('maximizeRestoreApp')
+})
+ipc.on('isMaximized', ()=> { changeMaxResBtn(true) })
+ipc.on('isRestored', ()=> { changeMaxResBtn(false) })
+
+//// CLOSE APP
+closeBtn.addEventListener('click', ()=>{
+    ipc.send('closeApp')
+
+})
+
+//// TOGGLE MENU
+// Expand and Retract
+showHideMenus.addEventListener('click', ()=>{
+    if(isLeftMenuActive){
+        mySidebar.style.width = '0px'
+        isLeftMenuActive = false
+    } else {
+        mySidebar.style.width = '280px'
+        isLeftMenuActive = true
+    }
+})
