fbb159e69cadae22c0a2ee91e10059b769ddf677
Spatzek <54891626+Spatzek@users.noreply.github.com>
2021-11-30 10:57:04 +0100

    Revert "updated data-service and fixed service key"

--- my-app/src/DAL/projectController.js
+++ my-app/src/DAL/projectController.js
@@ -9,7 +9,7 @@ admin.initializeApp({
 const db = admin.firestore();
 const project = require('../models/project');
 
-const createProject = async(req, res, next) => {
+const addProject = async(req, res, next) => {
     try{
         const data = req.body;
         await db.collection('projects').doc().set(data);
@@ -40,7 +40,7 @@ const getAllProjects = async(req, res, next) =>{
     }
 }
 
-const getProjectWithID = async(req, res, next) =>{
+const getProject = async(req, res, next) =>{
     try {
         const id = req.params.id;
         const project = await db.collection('projects').doc(id);
@@ -78,9 +78,9 @@ const deleteProject = async(req, res, next) =>{
 }
 
 module.exports = {
-    createProject,
+    addProject,
     getAllProjects,
-    getProjectWithID,
+    getProject,
     updateProject,
     deleteProject
 }
